e331492e56e7cbece47c1891c0461502eab2a3ab
Fabricio Mendonca Rodrigues <fabriciomendonca@gmail.com>
2017-08-31 01:08:31 -0300

    Playground

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'src')));
+app.use(express.static(path.resolve(__dirname, 'playground')));
 
 app.listen(3000, () => {
   console.log('Server iniciado na porta 3000');
